1be4cd74e0d864e3ce42086f3c00aa59034edc2c
M4dn4ss <sura0046@hotmail.com>
2023-03-13 10:32:22 +0300

    Data_Binding

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import {Movie} from '../movie';
 
 @Component({
     selector: 'movies',
-    templateUrl: 'movies.component.html',
-    styles: [`
-        h2{
-            color: blue;
-        }
-    `]
+    templateUrl: 'movies.component.html'
 })
 export class MoviesComponent{
+    title = 'Movie List';
 
+    movie: Movie = {
+        id: 1,
+        name: 'Movie Name'
+    }
+
+    getTitle(){
+        return this.title;
+    }
 }
